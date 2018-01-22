e30ad908de7f1fe96edb68fd4b8c1942ccce4ee2
Georgi Griffiths <georgi.griffiths@gmail.com>
2018-01-22 12:19:48 +0100

    polyfill for seneca-web 0.8.0

--- seneca-web-adapter-hapi.js
+++ seneca-web-adapter-hapi.js
@@ -9,6 +9,15 @@ module.exports = function hapi (options, context, auth, routes, done) {
     return done(new Error('no context provided'))
   }
 
+  context.ext('onPreAuth', function (request, reply) {
+    reply.seneca = request.seneca = seneca.root.delegate({
+      req$: request,
+      res$: reply,
+      tx$: seneca.root.idgen()
+    })
+    reply.continue()
+  })
+
   _.each(routes, (route) => {
     if (!route.auth && !route.secure) {
       unsecuredRoute(seneca, context, route)
@@ -23,18 +32,36 @@ module.exports = function hapi (options, context, auth, routes, done) {
 
 
 function handleRoute (seneca, request, reply, route) {
-  const payload = {
-    request$: request,
-    response$: reply,
-    args: {
-      isAuthenticated: request.auth.isAuthenticated,
-      body: request.payload,
-      route: route,
-      query: request.query,
-      params: request.params,
-      user: request.auth.credentials || null
-    }
+  reply.seneca = request.seneca = seneca.root.delegate({
+    req$: request,
+    res$: reply,
+    tx$: seneca.root.idgen()
+  })
+
+  var data = _.extend(
+    {},
+    _.isObject(request.body) ? request.body : {},
+    _.isObject(request.params) ? request.params : {},
+    _.isObject(request.query) ? request.query : {}
+  )
+
+  // This is what the seneca handler will get
+  // Note! request$ and response$ will be stripped
+  // if the message is sent over transport.
+  var payload = {
+    req$: request,
+    res$: reply,
+    data: data
+    // args: {
+    //   body: body,
+    //   route: route,
+    //   params: request.params,
+    //   query: request.query,
+    //   user: request.user || null,
+    //   headers: request.headers
+    // }
   }
+  console.log('Handle Route', route.pattern, payload.data)
 
   if (route.redirect) {
     return reply.redirect(route.redirect)
@@ -44,18 +71,43 @@ function handleRoute (seneca, request, reply, route) {
     return reply.redirect(route.auth.pass)
   }
 
-  seneca.act(route.pattern, payload, (err, response) => {
-    if (err) {
-      return reply(err)
-    }
+  if (route.build) {
+    route.build.call(request, reply, payload, act, reply)
+  }
+  else {
+    act(payload, reply)
+  }
 
-    if (route.autoreply) {
-      return reply(null, response)
-    }
-  })
+  function act (args, reply) {
+    reply.seneca.act(route.pattern, args, (err, response) => {
+      if (err) {
+        return reply(err)
+      }
+
+      // if (route.autoreply) {
+      //   return reply(null, response)
+      // }
+
+      if (!_.isObject(response)) return reply.send(response)
+
+      if (response.http$) {
+        var res = _.clone(response.http$)
+        delete response.http$
+        res.body = response
+        response = res
+      }
+      if (response.redirect) {
+        response.statusCode = response.statusCode || response.status || 302
+        return reply.redirect(response.redirect)
+      }
+      response.statusCode = response.statusCode || response.status
+      return reply(response.body || response)
+    })
+  }
 }
 
 function unsecuredRoute (seneca, context, route) {
+  console.log('ROUTE', route.path)
   context.route({
     method: route.methods,
     path: route.path,
@@ -66,14 +118,22 @@ function unsecuredRoute (seneca, context, route) {
 }
 
 function authRoute (seneca, context, route) {
+  console.log('AUTH_ROUTE', route.path)
   context.route({
     method: route.methods,
     path: route.path,
     config: {
-      auth: route.auth.strategy
+      auth: {
+        mode: 'try',
+        strategy: route.auth.strategy
+      }
     },
     handler: (request, reply) => {
-      handleRoute(seneca, request, reply, route)
+      if (request.auth.error) {
+        console.log(request.auth.error)
+        reply()
+      }
+      if (request.auth.isAuthenticated) handleRoute(seneca, request, reply, route)
     }
   })
 }
